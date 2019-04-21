4ed8a3a35307faeae580140a226103c0ca8b2f3e
Dhandapani299 <49011974+Dhandapani299@users.noreply.github.com>
2019-04-21 01:23:22 -0700

    Add files via upload

---
+++ Arr.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+public class Arr {
+ 
+ 
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+ 
+		
+		System.out.print("Enter how many numbers: ");
+		int numOfFriends = Integer.parseInt(scan.nextLine());
+ 
+		//Create a string array to store the names of your friends
+		String arrayOfNames[] = new String[numOfFriends];
+		for (int i = 0; i < arrayOfNames.length; i++) {
+			System.out.print( "  ");
+		        arrayOfNames[i] = scan.nextLine();
+		}
+		    
+		
+		for (int i = 0; i < arrayOfNames.length; i++) {
+			
+		        System.out.print(arrayOfNames[i] + i + "\n" );
+		}
+		  
+ 
+		    
+ 
+	}
+ 
+}
